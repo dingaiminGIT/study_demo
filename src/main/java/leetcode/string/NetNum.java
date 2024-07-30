84f0a74ff49ce26ae16d40c9a3e31a20b4080874
aiminding <aiminding@tencent.com>
2024-07-30 23:27:03 +0800

    修改域名统计，便于理解

--- src/main/java/leetcode/string/NetNum.java
+++ src/main/java/leetcode/string/NetNum.java
@@ -9,22 +9,22 @@ import java.util.Map;
  * 做一个简单的网站统计分析，规则如下
  * 输入若干字符串
  * "333 abc.yuanfudao.com"
- *
+ * <p>
  * 各级域名的计数
  * 333 .com
  * 333 yuanfudao.com
  * 333 abc.yuanfudao.com
- *
+ * <p>
  * input List<String>
  * 3 a.yuanfudao.com
  * 1 b.yuanfudao.com
- *
+ * <p>
  * output List<String>
  * 3 a.yuanfudao.com
  * 1 b.yuanfudao.com
  * 4 com
  * 4 yuanfudao.com
- *
+ * <p>
  * 猿辅导
  *
  * @author: dingaimin
@@ -39,7 +39,7 @@ public class NetNum {
 
         NetNum test = new NetNum();
         List<String> output = test.output(input);
-        for(String str : output) {
+        for (String str : output) {
             System.out.println(str);
         }
     }
@@ -49,7 +49,7 @@ public class NetNum {
         Map<String, Integer> map = new HashMap<>();
 
         // 遍历 input 解析计数和域名
-        for(String str : input) {
+        for (String str : input) {
             // 先根据空格拆分出技术和完整域名
             String[] numAndNet = str.split(" ");
             // 先获取数量
@@ -58,17 +58,17 @@ public class NetNum {
             String net = numAndNet[1];
 
             // 拆分域名
-            String[] nets =  net.split("\\.");
+            String[] nets = net.split("\\.");
             int length = nets.length;
             //  拼接子域名，并放到域名List里
             List<String> netList = new ArrayList<>(length);
             StringBuilder strBuffer = new StringBuilder();
-            for(int i = length - 1; i >= 0; i--) {
+            for (int i = length - 1; i >= 0; i--) {
                 // 为了保证域名的顺序的正确性，采用从头插入的方式
-                if (i != length - 1) {
-                    strBuffer.insert(0, nets[i] + ".");
-                } else {
+                if (i == length - 1) {
                     strBuffer.insert(0, nets[i]);
+                } else {
+                    strBuffer.insert(0, nets[i] + ".");
                 }
                 netList.add(strBuffer.toString());
             }
@@ -77,7 +77,7 @@ public class NetNum {
 
         // 返回结果，遍历 map ，拼接返回结果
         List<String> res = new ArrayList<>();
-        for(String key : map.keySet()) {
+        for (String key : map.keySet()) {
             String out = map.get(key) + " " + key;
             res.add(out);
         }
@@ -86,8 +86,8 @@ public class NetNum {
 
     public void helper(List<String> netList, Integer num, Map<String, Integer> map) {
         // 对域名进行计数的一个工具类
-        for(String net : netList) {
-            if(map.containsKey(net)) {
+        for (String net : netList) {
+            if (map.containsKey(net)) {
                 // 存在进行累加
                 Integer netNum = map.get(net);
                 map.put(net, netNum + num);
