e83cdf58db99d7e0837dc9acd4e16594272b6bbb
aiminding <aiminding@tencent.com>
2024-08-12 15:19:04 +0800

    update

--- src/main/java/leetcode/linkedlist/HasCycle.java
+++ src/main/java/leetcode/linkedlist/HasCycle.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 /**
  * 给定一个链表，判断链表中是否有环。
+ * keep用户增长一面
  * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/linked-list-cycle
